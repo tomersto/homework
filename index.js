323ed4341c86d541b05373fe1df10abbe4e9bc0b
galamo <galamouyal88@gmail.com>
2020-06-15 22:33:14 +0300

    after review

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 (function (){
     console.log("this function works")
-    console.log("this function works")
-    console.log("this function works")
-    console.log("this function works")
-    console.log("this function works")
-    console.log("this function works")
+    const a = 1;
+    console.log(a)
+
     
 })()
